b6f77c0d63fec8bb82013d040c3a3db1fb8e5977
dhananjay9309 <59719022+dhananjay9309@users.noreply.github.com>
2020-01-15 16:53:26 +0530

    Create ap1.py

---
+++ lab1/ap1.py
@@ -0,0 +1,30 @@
+i=4
+while i:
+    print(i)
+    i-=1
+    
+    
+if i==0:
+    print("yes i=0")
+else:
+    print("i is not 0")
+
+i = 2.334
+print(i)
+
+i=str(i)
+topic='Python'
+print(i+ topic)
+
+lst=['this is list ',100]
+print(lst)
+
+tupple=('this is tuple',20)
+print(tupple)
+
+i=4
+def cal(i):
+    i=i%2
+i=cal(i)
+print(i)
+    
